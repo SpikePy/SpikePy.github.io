ba9e899d01e1d99dd209b896d83f1b4a287f71f3
Rene Hoffmann <hoffelmann@gmail.com>
2019-04-08 10:57:36 +0200

    improved wing tsun MVP

--- WingChun/script.js
+++ WingChun/script.js
@@ -1,6 +1,6 @@
-var commands_text    = ['Links: Tan Sau', 'Rechts: Tan Sau', 'Links: Gan Sau', 'Rechts: Gan Sau', 'Links: Pak Sau', 'Rechts: Pak Sau', 'Links: Scham Sau', 'Rechts: Scham Sau', 'Links: Bong Sau', 'Rechts: Bong Sau']
-var commands_audio   = ['tan_links.mp3',  'tan_rechts.mp3',  'gan_links.mp3',  'gan_rechts.mp3',  'pak_links.mp3',  'pak_rechts.mp3',  'scham_links.mp3',  'scham_rechts.mp3',  'bong_links.mp3',  'bong_rechts.mp3']
-var commands_images  = ['tan.jpg',        'tan.jpg',         'gan.jpg',        'gan.jpg',         'pak.jpg',        'pak.jpg',         'scham.jpg',        'scham.jpg',         'bong.jpg',        'bong.jpg']
+var commands_text    = ['Links: Tan Sao', 'Rechts: Tan Sao', 'Links: Gan Sao', 'Rechts: Gan Sao', 'Links: Pak Sao', 'Rechts: Pak Sao', 'Links: Cham Sao', 'Rechts: Cham Sao', 'Links: Bong Sao', 'Rechts: Bong Sao']
+var commands_audio   = ['tan_links.mp3',  'tan_rechts.mp3',  'gan_links.mp3',  'gan_rechts.mp3',  'pak_links.mp3',  'pak_rechts.mp3',  'cham_links.mp3',  'cham_rechts.mp3',  'bong_links.mp3',  'bong_rechts.mp3']
+var commands_images  = ['tan.jpg',        'tan.jpg',         'gan.jpg',        'gan.jpg',         'pak.jpg',        'pak.jpg',         'cham.jpg',        'cham.jpg',         'bong.jpg',        'bong.jpg']
 
 function commands_random() {
   var body   = document.getElementsByTagName('body');
