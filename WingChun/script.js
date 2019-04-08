1f2cca6e782584639392fb354527afd98a7d5b5b
Rene Hoffmann <hoffelmann@gmail.com>
2019-04-08 20:53:09 +0200

    finished new version

--- WingChun/script.js
+++ WingChun/script.js
@@ -10,68 +10,64 @@ const database_technique = [
   ['Bong Sao', 'bong.mp3', 'bong.jpg'],
 ];
 
-
-// Bind html elements to variables after body is loaded for later manipulation
-function onload() {
-  const input  = document.getElementById('repetitions');
-  const select = document.getElementById('select');
-  const button = document.getElementById('button');
-  const text   = document.getElementById('text')
-  const image  = document.getElementById('image');
-}
-
 // Entry point, gets started if button is pressed
 function start() {
-  html_toggle_1();
+  html("running");
   let audio  = document.getElementById('player');
   
   let commands_array = [];
-  for (let index = 0; index < get_repetitions() * 6; index += 6) {
+  for (let index = 0; index < get_repetitions() * 4; index += 4) {
     let random_side      = Math.round(Math.random());
     let random_technique = Math.floor(Math.random() * get_difficulty());
     
     commands_array[index+0] = database_side[random_side][0] + database_technique[random_technique][0];
     commands_array[index+1] = "images/" + database_technique[random_technique][2];
-    commands_array[index+2] = "audio/"  + database_side[random_side][1];
-
-    commands_array[index+3] = database_side[random_side][0] + database_technique[random_technique][0];
-    commands_array[index+4] = "images/" + database_technique[random_technique][2];
-    commands_array[index+5] = "audio/"  + database_technique[random_technique][1];
+    commands_array[index+2] = "audio/side/"  + database_side[random_side][1];
+    commands_array[index+3] = "audio/technique/"  + database_technique[random_technique][1];
   }
   
   console.table(commands_array);
 
   let index = 0;
+  let toggle = 1; //toggles between side and technique
   text.innerHTML = commands_array[index+0];
   image.src      = commands_array[index+1];
   audio.src      = commands_array[index+2];
   audio.play();
   audio.onended = function() {
-    if(index+3 < commands_array.length) {
-      index += 3;
-      text.innerHTML = commands_array[index+0];
-      image.src      = commands_array[index+1];
-      audio.src      = commands_array[index+2];
-      audio.play(); 
-    } else {
-      html_toggle_2();
+    if(index < commands_array.length) {
+      if(toggle == 0){
+        setTimeout(function(){
+          text.innerHTML = commands_array[index+0];
+          image.src      = commands_array[index+1];
+          audio.src      = commands_array[index+2];
+          audio.play();
+        },get_speed());
+        toggle = 1;
+      } 
+      else {
+        audio.src      = commands_array[index+3];
+        audio.play();
+        toggle = 0;
+        index += 4;
+      }
+    } 
+    else {
+      html("reset");
     }
   }
 }
 
-// remove not needed html elements
-function html_toggle_1() {
-  button.style.display = "none";
-  select.style.display = "none";
-  document.getElementById('repetitions').style.display  = "none";
-}
-
-// show hidden html elements
-function html_toggle_2() {
-  button.style.display = "inline";
-  select.style.display = "inline";
-  document.getElementById('repetitions').style.display  = "inline";
-  text.innerHTML = "";
+// manipulate html depending on state
+function html(state) {
+  if (state == "running") {
+    interaction.style.display = "none";
+  }
+  else if (state == "reset") {
+    interaction.style.display = "block";
+    image.src      = "images/hand.jpg"
+    text.innerHTML = "";
+  }
 }
 
 // Get value of html-select to decides which elements of *database_commands_array* 
@@ -80,7 +76,12 @@ function get_difficulty() {
   return select.options[select.selectedIndex].value;
 }
 
+// Get waitingg time between commands
+function get_speed() {
+  return speed.options[speed.selectedIndex].value;
+}
+
 // Get value of repetitions input field
 function get_repetitions() {
-  return document.getElementById('repetitions').value;
+  return repetitions.value;
 }
