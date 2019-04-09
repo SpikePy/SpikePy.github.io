ea4f21e5637f5c758d9ddddfdfad97bcfde86168
Rene Hoffmann <hoffelmann@gmail.com>
2019-04-09 10:23:08 +0200

    refactore logic to make it more read-/understandable

--- WingChun/script.js
+++ WingChun/script.js
@@ -10,29 +10,34 @@ const database_technique = [
   ['Bong Sao', 'bong.mp3', 'bong.jpg'], // └─────── Meister
 ];
 
+wait_between     =  800; // Time to wait between playing side and technique audio files
+wait_fullCommand = 1500; // Time to wait between 2 commands
+
 // Entry point, gets started if button is pressed
 function start() {
   html("running");
 
-  let toggle = 1; //toggles between side and technique
-  command_1();
-  audio.onended = function() {
-    let repetition = toggle / 2;
-    if(repetition < get_repetitions()) {
-      if(toggle % 2 == 0){
-        setTimeout(function(){ // Wait for given time till command_1 is started
-          command_1();
-        },get_waitTime());
-      }
-      else {
-        command_2();
-      }
-      toggle++;
-    } 
-    else {
-      html("reset");
-    }
+  for (repetition = 0; repetition < get_repetitions(); repetition++) {
+    setTimeout(function() { // Wait time before starting loop
+      random_side      = Math.round(Math.random());
+      random_technique = Math.floor(Math.random() * get_difficulty());
+      text.innerHTML   = database_side[random_side][0] + database_technique[random_technique][0];
+      console.log(text.innerHTML)
+      image.src        = "images/" + database_technique[random_technique][2];
+      audio.src        = "audio/side/"  + database_side[random_side][1];
+      audio.play();
+
+      setTimeout(function() {
+        audio.src      = "audio/technique/" + database_technique[random_technique][1];
+        audio.play();
+      },wait_between);
+    },repetition * get_waitTime());
   }
+
+  // Wait repetition * get_waitTime before resetting the page
+  setTimeout(function() {
+    html("reset");
+  },repetition * get_waitTime());
 }
 
 // manipulate html depending on state
@@ -55,7 +60,8 @@ function get_difficulty() {
 
 // Get waitingg time between commands
 function get_waitTime() {
-  return wait.options[wait.selectedIndex].value;
+  // Time to wait between 2 commands + offset chosen in html
+  return wait_fullCommand + Number(wait.options[wait.selectedIndex].value);
 }
 
 // Get value of repetitions input field
